dd6eba1ec9eed4fe5e546ec3b8b43b56d793d351
Rich Smith <rich@richjdsmith.ca>
2018-11-21 10:59:38 +0000

    Updated so flash message spans screen.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,5 +3,10 @@ class PagesController < ApplicationController
   # before_action :authenticate_user!, only: :dashboard
   skip_after_action :verify_authorized
   def index
+    flash[:notice] = "This is a notice" 
+   flash[:alert] = "This is an alert" 
+   flash[:warning] = "This is an warning" 
+   flash[:error] = "This is an error" 
+   flash[:success] = "This is an success" 
   end
 end
