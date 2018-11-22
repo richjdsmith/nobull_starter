85ea6196341d590fcea9ec613afdd4b494d3b0f7
Rich Smith <rich@richjdsmith.ca>
2018-11-22 18:49:52 +0000

    Set up pages_layout, cleaned up devise_layout, cleaned up application_layout and built the pages_layout base as well as a basic index page for the pages controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,23 @@
-Rails.application.routes.draw do
-  
+Rails.application.routes.draw do 
   # All magic devise controller. 
   devise_for :users
+  # Root for authenticated users.
+  authenticated :user do
+    root :to => 'dashboard#show', as: :authenticated_root
+  end
+  # Root for non-auth users.
+  root :to => 'pages#index'
+
   # Administrate Admin Dashboards
   namespace :admin do
     resources :users
     root to: "users#index"
   end
 
+  # Derp.
   resources :pages, only: :index
+  # Derpp.
   resource :dashboard, controller: 'dashboard', only: :show
-  authenticated :user do
-    root :to => 'dashboard#show', as: :authenticated_root
-  end
-  root :to => 'pages#index'
+  
 
 end
