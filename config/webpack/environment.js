04c3e8217b9ed5eba73b41206c13c32d50c2f527
Rich Smith <rich@richjdsmith.ca>
2018-11-23 22:01:50 +0000

    Fixed all sorts of purgecss issues. Fml.

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,6 +2,9 @@ const {
   environment
 } = require('@rails/webpacker')
 const erb = require('./loaders/erb')
+// const path = require('path')
+// const PurgecssPlugin = require('purgecss-webpack-plugin')
+// const glob = require('glob-all')
 
 environment.loaders.append('erb', erb)
 
