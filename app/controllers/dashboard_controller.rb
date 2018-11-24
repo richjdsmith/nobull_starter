b025b321310f819f53971381625653cfffd872ee
Rich Smith <rich@richjdsmith.ca>
2018-11-24 16:02:26 +0000

    Added empty state to dashboard.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!, only: :show
   skip_after_action :verify_authorized
   def show
-    flash[:notice] = "You're one sexy beast!"
+    # flash[:notice] = "You're one sexy beast!"
   end
 end
